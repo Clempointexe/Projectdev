2bd65f6787eb440cf578ad2b0cdde373cddce749
clempointexe <cmbiteau@gmail.com>
2022-05-18 11:05:21 +0200

    init

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 /** Setup app */
+// TEST FOR FIRST COMMIT
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
